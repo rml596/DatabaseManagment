56f234fd0f186927331efe573d5aa93eb64aaed3
Robert Lynch <rml596@gmail.com>
2016-11-29 20:56:16 -0500

    Lab 10

---
+++ Lab 10.sql
@@ -0,0 +1,46 @@
+﻿/*Question 1
+func1on PreReqsFor(courseNum) - Returns the immediate prerequisites for the passed-in course number.*/
+
+CREATE OR REPLACE FUNCTION PreRequesFor(int, refcursor) RETURNS refcursor AS
+$$
+DECLARE 
+	courseInput int := $1;
+	resultSet refcursor := $2;
+BEGIN
+	OPEN resultSet FOR
+		SELECT courseNum, preReqNum
+		FROM Prerequisites, Courses
+		WHERE num = PreReqNum
+		AND courseInput = courseNum;
+	RETURN resultSet;
+END;
+
+$$
+LANGUAGE plpgsql;
+
+SELECT PreRequesFor(221, 'results');
+FETCH ALL FROM results;
+
+
+/*Question 2
+func1on IsPreReqFor(courseNum) - Returns the courses for which the passed-in course number is an immediate pre-requisite.*/
+
+CREATE OR REPLACE FUNCTION IsPreReqsFor(int, refcursor) RETURNS refcursor AS
+$$
+DECLARE 
+	courseInput int := $1;
+	resultSet refcursor := $2;
+BEGIN
+	OPEN resultSet FOR
+		SELECT preReqNum, courseNum
+		FROM Prerequisites, Courses
+		WHERE courseNum = num
+		AND courseInput = PreReqNum;
+	RETURN resultSet;
+END;
+
+$$
+LANGUAGE plpgsql;
+
+SELECT IsPreReqsFor(220, 'results');
+FETCH ALL FROM results;
